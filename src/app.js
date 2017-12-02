fe13bdb99bc36a9263b2e027c9e8e1354c2005d9
Franco Arolfo <francoarolfo@hotmail.com>
2017-12-02 18:24:33 -0300

    Refactor on hirerarchy

--- src/app.js
+++ src/app.js
@@ -1,12 +1,19 @@
 $(function() {
-        var ongs = [
+
+        var searchSettings = {
+            text: '',
+            category: 'all'
+        };
+
+        var allOngs = [
         	{
         		name: 'AntiWar',
         		bitcoinAddress: '1M87hiTAa49enJKVeT9gzLjYmJoYh9V98',
         		website: 'https://antiwar.com',
         		bitcoinAddressWebsite: 'https://antiwar.com/aapledge/bitcoin-donate.html',
         		slogan: 'Your best source for antiwar news, viewpoints and activites.',
-        		image: 'antiwar.jpg'
+        		image: 'antiwar.jpg',
+                categories: ['people-health', 'world-peace']
         	},
         	{
         		name: 'Tunapanda',
@@ -14,7 +21,8 @@ $(function() {
         		website: 'http://www.tunapanda.org/',
         		bitcoinAddressWebsite: 'http://www.tunapanda.org/',
         		slogan: 'Help us to spread digital era vocational training and increase personal freedom through self-expression.',
-        		image: 'tunapanda.jpg'
+        		image: 'tunapanda.jpg',
+                categories: ['technology', 'education']
         	},
         	{
         		name: 'Run 2 Rescue',
@@ -22,7 +30,8 @@ $(function() {
         		website: 'http://run2rescue.com',
         		bitcoinAddressWebsite: 'http://run2rescue.com/bitcoin.html',
         		slogan: 'Run 2 Rescue is a Christian organization that will reach, rescue and restore victims of Human Trafficking.',
-        		image: 'run2rescue.jpg'
+        		image: 'run2rescue.jpg',
+                categories: ['people-health']
         	},
         	{
         		name: 'The Water Project',
@@ -30,7 +39,8 @@ $(function() {
         		website: 'https://thewaterproject.org/',
         		bitcoinAddressWebsite: 'https://thewaterproject.org/give-water',
         		slogan: 'We are committed to bringing people together to solve the problem of finding clean water.',
-        		image: 'thewaterproject.jpg'
+        		image: 'thewaterproject.jpg',
+                categories: ['people-health']
         	},
         	{
         		name: 'OMNI NANO',
@@ -38,7 +48,8 @@ $(function() {
         		website: 'https://omninano.org/',
         		bitcoinAddressWebsite: 'https://omninano.org/donate-to-stem-education/',
         		slogan: 'We are a non-profit organization teaching students cutting-edge science, specifically nanotechnology.',
-        		image: 'omninano.jpg'
+        		image: 'omninano.jpg',
+                categories: ['computer-science', 'technology', 'education']
         	},
         	{
         		name: 'Sean\'s Outpost',
@@ -46,7 +57,8 @@ $(function() {
         		website: 'http://www.seansoutpost.com/',
         		bitcoinAddressWebsite: 'http://www.seansoutpost.com/',
         		slogan: 'Over 167,360 meals fed to the homeless. With Bitcoin.',
-        		image: 'seansoutpost.jpg'
+        		image: 'seansoutpost.jpg',
+                categories: ['people-health']
         	},
         	{
         		name: 'Songs of Love',
@@ -54,7 +66,8 @@ $(function() {
         		website: 'https://www.songsoflove.org',
         		bitcoinAddressWebsite: 'https://www.songsoflove.org/cryptocurrencies/',
         		slogan: 'The Songs of Love Foundation is a national nonprofit 501(c)(3) organization that creates free, personalized, original songs to uplift children and teens currently facing tough medical, physical or emotional challenges.',
-        		image: 'songsoflove.png'
+        		image: 'songsoflove.png',
+                categories: ['people-health', 'technology']
         	},
         	{
         		name: 'Free Software Foundation',
@@ -62,7 +75,8 @@ $(function() {
         		website: 'http://www.fsf.org/',
         		bitcoinAddressWebsite: 'https://my.fsf.org/donate',
         		slogan: 'The Free Software Foundation (FSF) is a nonprofit with a worldwide mission to promote computer user freedom. We defend the rights of all software users.',
-        		image: 'freesoftwarefoundation.png'
+        		image: 'freesoftwarefoundation.png',
+                categories: ['computer-science', 'technology']
         	},
         	{
         		name: 'Human Rights Foundation',
@@ -70,14 +84,55 @@ $(function() {
         		website: 'https://humanrights.foundation/',
         		bitcoinAddressWebsite: 'https://humanrights.foundation/donate/',
         		slogan: 'Human Rights Foundation is an independent association with the mission to effectively promote and protect all universally recognized human rights and fundamental freedoms regardless of linguistic or geographic barriers.',
-        		image: 'humanrightsfoundation.jpg'
+        		image: 'humanrightsfoundation.jpg',
+                categories: ['people-health']
         	}
         ];
 
-        ongs.forEach(function(ong) {
-        	$('.coverflow').append("<div class=\"cover\" style=\"background: url(" + ong.image + ") no-repeat center center; background-size: cover;\"><span class=\"header\">" + ong.name + "</span><span class=\"footer\">" + ong.slogan + "</span></div>");
-        })
+        function populateList(ongs) {
+            ongs.forEach(function(ong) {
+                $('.coverflow').append("<div class=\"cover\" style=\"background: url(" + ong.image + ") no-repeat center center; background-size: cover;\"><span class=\"header\">" + ong.name + "</span><span class=\"footer\">" + ong.slogan + "</span></div>");
+            });
 
-        $('.coverflow').coverflow();
+            $('.coverflow').coverflow();        
+        }
 
-    });
+        window.search = function() {
+            searchSettings.text = $('#search-text').text();
+            clearList();
+            populateList(filterBySearch());
+        }
+
+        function clearList() {
+            $('.coverflow').html('');
+        }
+
+        function filterBySearch() {
+            return _.filter(allOngs, function(item) {
+                return containsText(item, searchSettings.text.toLowerCase()) || fitsCategory(item, searchSettings.category);
+            });
+        }
+
+        function containsText(item, text) {
+            if (text == '') {
+                return true;
+            }
+
+            return item.name.toLowerCase().search(text) != -1 || item.slogan.toLowerCase().search(text) != -1;
+        }
+
+        function fitsCategory(item, category) {
+            if (category == 'all') {
+                return true;
+            }
+
+            return item.categories.includes(category);
+        }
+
+        populateList(allOngs);
+
+        window.setCategory = function(category) {
+            searchSettings.category = category;
+            $('#category').text(event.srcElement.text);
+        }
+});
